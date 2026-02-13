34530c8dd7467498f147308814f71ca0b37c8535
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2026-02-13 18:52:13 +0000

    Upload: pt-br.lua

---
+++ locales/pt-br.lua
@@ -0,0 +1,60 @@
+-- If you find any translation mistaktes, feel free to let us know so we can correct them!
+-- Se você encontrar algum erro de tradução, sinta-se à vontade para nos informar para que possamos corrigi-lo!
+
+Locales = Locales or {}
+
+Locales["pt-br"] = {
+    ["open_garage"] = "Pressione [E] para abrir a garagem",
+    ["open_impound"] = "Pressione [E] para abrir o deposito",
+    ["park_vehicle"] = "Pressione [E] para estacionar o veiculo",
+    ["vehicle_not_owned"] = "Este veículo não é seu!",
+    ["already_out"] = "Este veículo já esta fora da garagem!",
+    ["too_far_away"] = "Você está muito longe da garagem!",
+    ["error"] = "Ocorreu um erro!",
+    ["incorrect_type"] = "Você não pode estacionar esse tipo de veiculo nesta garagem!",
+    ["no_vehicles_available"] = "Você não tem veículos armazenados aqui!",
+
+    -- UI
+    ["no_vehicles"] = "Parece que você não tem nenhum veículo nesta garagem ou não possui nenhum veículo!",
+    ["speed"] = "Velocidade",
+    ["acceleration"] = "aceleração",
+    ["handling"] = "Manuseio",
+    ["braking"] = "freio",
+    ["front"] = "Frente",
+    ["side"] = "Lado",
+    ["top"] = "Topo",
+    ["take_out"] = "Retirar",
+    ["search_placeholder"] = "Procurar",
+    ["class"] = "Classes",
+    ["only_favorites"] = "Favoritos",
+    ["yes"] = "sim",
+    ["no"] = "não",
+    ["fuel"] = "combustivel",
+    ["health"] = "Condição",
+
+    -- Vehicle Classes:
+
+    ["all"] = "Todos",
+    ["compacts"] = "Compactos",
+    ["sedans"] = "Sedans",
+    ["suvs"] = "SUVs",
+    ["coupes"] = "Coupes",
+    ["muscle"] = "Muscle",
+    ["sports_classics"] = "Sports Classics",
+    ["sports"] = "Sports",
+    ["super"] = "Super",
+    ["motorcycles"] = "Motos",
+    ["off_road"] = "Off-road",
+    ["industrial"] = "Industrial",
+    ["utility"] = "Utilidade",
+    ["vans"] = "Vans",
+    ["cycles"] = "Bicicletas",
+    ["boats"] = "Barcos",
+    ["helicopters"] = "Helicopteros",
+    ["planes"] = "Aviões",
+    ["service"] = "Serviço",
+    ["emergency"] = "Emergencia",
+    ["military"] = "Militar",
+    ["commercial"] = "Commercial",
+    ["open_wheel"] = "formula 1",
+}
